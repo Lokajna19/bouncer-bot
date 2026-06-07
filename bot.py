71f61640758f61cb863529d7968c3d7c8be3636e
B Lokajna Reddy <285201762+Lokajna19@users.noreply.github.com>
2026-06-07 19:55:33 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        age=int(input("What is your age ? \n"))
+    except ValueError:
+        print("Invalid Input")
+        continue
+    name=input("Enter your name ? \n")
+        
+    if age >= 18:
+            
+         print(f"{name} is eligible to vote in india!")
+         continue
+    else:
+         print(f"{name} is not eligible to vote in india!")
+         continue
+    
